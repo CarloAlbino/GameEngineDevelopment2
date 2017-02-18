40c68b8430cfce8ab78c2c5ef8a42954341e8f86
CarloAlbino <carloalbino@gmail.com>
2017-02-18 00:52:45 -0500

    Fixed node rotation, cleaned up main

--- Completed__SceneGraph_version_one/OpenGL_Scene_Node_Implementation_version_one/OpenGL_Starter_Kit/Starter_Class.cpp
+++ Completed__SceneGraph_version_one/OpenGL_Scene_Node_Implementation_version_one/OpenGL_Starter_Kit/Starter_Class.cpp
@@ -3,9 +3,6 @@
 #include "Triangle.h"
 #include "Tank.h"
 
-
-
-
 float rotation_angle = 0.0f;
 float x, y=0;
 
@@ -14,15 +11,13 @@ void myDisplay(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	//Ready to Draw
-	glColor3f(1.0, 1.0, 0.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	
-	
-	
-	Tank redTank = Tank(glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0)), 0.1);
-	redTank.setColor(1.0, 0.0, 0.0);
-	redTank.render();
+
+	glm::mat4 rot = glm::rotate(glm::mat4(1.0), rotation_angle, glm::vec3(0, 0, 1));
+	Tank* redTank = new Tank(rot, 0.1);
+	redTank->setColor(1.0, 0.0, 0.0);
+	redTank->render();
 
 	Tank blueTank = Tank(glm::translate(glm::mat4(1.0f), glm::vec3(-x, y, 0)), 0.1);
 	blueTank.setColor(0.0, 0.0, 1.0);
@@ -36,36 +31,9 @@ void myDisplay(void)
 	whiteTank.setColor(1.0, 1.0, 1.0);
 	whiteTank.render();
 
-
 	Tank bigTank = Tank(glm::translate(glm::mat4(1.0f), glm::vec3(x, 0, 0)), 0.2);
 	bigTank.setColor(1.0, 0.0, 1.0);
 	bigTank.render();
-
-
-
-	/*
-	Square root = Square(glm::mat4(1.0f), 0.5);
-	root.setColor(1.0, 1.0, 0.0);
-	
-	
-	//Triangle t = Triangle(glm::rotate(glm::mat4(1.0), 30.0f, glm::vec3(0.0f, 0.0f, 1.0f)), 0.7);
-	Triangle t = Triangle(glm::translate(glm::mat4(1.0), glm::vec3(0.5f, 0.5f, 0.0f)), 0.5);
-
-	t.setColor(0.0, 1.0, 0.0);
-	root.addChild(&t);
-
-	//Triangle t2 = Triangle(glm::rotate(glm::mat4(1.0), 0.0f, glm::vec3(0.0f, 0.0f, 1.0f)), 0.3);
-	Square t2 = Square(glm::translate(glm::mat4(1.0), glm::vec3(-0.5f, -0.5f, 0.0f)), 0.5);
-	t2.setColor(1.0, 0.0, 0.0);
-	root.addChild(&t2);
-	
-	Triangle t3 = Triangle(glm::translate(glm::mat4(1.0), glm::vec3(0.0f, -0.5f, 0.0f)), 0.1);
-	t3.setColor(0.0, 0.0, 1.0);
-	root.addChild(&t3);
-	
-
-	root.render();
-	*/
 
 	glFlush();
 }
@@ -73,7 +41,7 @@ void myDisplay(void)
 
 void myIdleFunc()
 {
-	//rotation_angle += 0.01;
+	rotation_angle += 0.001;
 	x += 0.0001;
 	y -= 0.0001;
 	glutPostRedisplay();
